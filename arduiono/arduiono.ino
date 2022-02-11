f94c2dcd2b1cfcb96cd3ac945ff2758afb30a89e
Jiří Pinkava <jiri.pinkava@rossum.ai>
2022-02-11 19:07:44 +0100

    working arduino project for light off command

--- arduiono/arduiono.ino
+++ arduiono/arduiono.ino
@@ -1,37 +1,39 @@
-int led_pin = 3;
+int LED_PIN = 3;
 
 const char OFF[] = "110110110110111101101011011110101011011011010111111111111011111110111110111011110101101";
-const int T_mod_us = 26;
-const int T_pulse_us = 420;
+const int MOD_FREQ = 38285;
+const int T_pulse_us = 422;
 const int T_pulse_init1_us = 3500;
-const int T_pulse_init2_us = 1600;
+const int T_pulse_init2_us = 1650;
+unsigned long t;
 
-void send_half_bit(char c, int len_us) {
-  for (int i = 0; i < len_us / T_mod_us; ++i) {
-    digitalWrite(led_pin, c);
-    delayMicroseconds(T_mod_us);  
-    digitalWrite(led_pin, LOW);
-    delayMicroseconds(T_mod_us);  
+
+static void send_half_bit(const bool c, const int len_us) {
+  t += len_us;
+  if (c) {
+    tone(LED_PIN, MOD_FREQ);
   }
+  while (t > micros());
+  noTone(LED_PIN);
 }
 
-void send_bit(char c) {
-  send_half_bit(c == "1" ? HIGH : LOW, T_pulse_us);
+static void send_bit(const char c) {
+  send_half_bit(c == '1' ? HIGH : LOW, T_pulse_us);
   send_half_bit(LOW, T_pulse_us);
 }
 
 void setup() {
-  pinMode(led_pin, OUTPUT);
-  digitalWrite(led_pin, LOW);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
-  send_half_bit("1", T_pulse_init1_us);
-  send_half_bit("0", T_pulse_init2_us);
-  
+  t = micros();
+  send_half_bit(HIGH, T_pulse_init1_us);
+  send_half_bit(LOW, T_pulse_init2_us);
   for (int i = 0; i < sizeof(OFF) - 1; i++) {
     send_bit(OFF[i]);
   }
 
-  delay(200);
+  delay(300);
 }
